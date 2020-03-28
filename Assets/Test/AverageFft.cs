99b7c3a8a58d564d668d056f6c20ee5375fe32bf
Jeremy <peace@trippyogi.com>
2020-03-27 23:44:31 -0700

    Add AverageFftJob

--- Assets/Test/AverageFft.cs
+++ Assets/Test/AverageFft.cs
@@ -1,100 +1,105 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using Unity.Collections;
+﻿using Unity.Collections;
 using Unity.Jobs;
 using Unity.Mathematics;
 using UnityEngine;
 
-public class AverageFft : MonoBehaviour
+namespace Assets.Test
 {
-    public NativeArray<float> Averages => _averages;
-    private NativeArray<float> _averages { get; }
-    private NativeArray<float> _input { get; }
-    private readonly int _numBins;
-
-    private const float SampleRate = 48000;
-
-    public AverageFft(int numBins)
+    public class AverageFft : MonoBehaviour
     {
-        _numBins = numBins;
-        _averages = new NativeArray<float>(numBins, Allocator.Persistent);
-    }
+        private NativeArray<float> Input { get; }
+        private readonly int _numBins;
+        private const float SampleRate = 48000;
 
-    public float[] CalculateAverages(NativeArray<float> input, int numBins)
-    {
-        _input.CopyFrom(input);
-        // Log based averaging, which closely resembles how humans perceive sound
-        const int minBandwidth = 60;
-        var nyq = SampleRate / 2.0f;
-        var octaves = 1;
-        // Log averaging algorithm returns one less band
-        //numBins++;
-
-        while ((nyq /= 2) > minBandwidth)
+        public AverageFft(NativeArray<float> input, int numBins)
         {
-            octaves++;
+            Input = input;
+            _numBins = numBins;
         }
 
-        var bandsPerOctave = (float) numBins / octaves;
-        var averages = new float[numBins];
-        for (var i = 0; i < octaves; i++)
+        public NativeArray<float> CalculateAverages([ReadOnly] NativeArray<float> input)
         {
-            float lowFreq;
-            if (i == 0)
+            Input.CopyFrom(input);
+            // Log based averaging, which closely resembles how humans perceive sound
+            const int minBandwidth = 60;
+            var nyq = SampleRate / 2.0f;
+            var octaves = 1;
+            //// Log averaging algorithm returns one less band
+            //numBins++;
+
+            while ((nyq /= 2) > minBandwidth)
             {
-                lowFreq = 0;
-            }
-            else
-            {
-                lowFreq = (SampleRate / 2) / (float) math.pow(2, octaves - i);
+                octaves++;
             }
 
-            var hiFreq = (SampleRate / 2) / (float) math.pow(2, octaves - i - 1);
-            var freqStep = (hiFreq - lowFreq) / bandsPerOctave;
-            var f = lowFreq;
-            for (var j = 0; j < bandsPerOctave; j++)
-            {
-                var offset = (int) (j + i * bandsPerOctave);
-                averages[offset] = CalculateAvg(f, f + freqStep);
-                f += freqStep;
-            }
+            var bandsPerOctave = _numBins / octaves;
+            var averages = new NativeArray<float>(_numBins, Allocator.Persistent);
+
+            new AverageFftJob()
+                {Input = input, Averages = averages, Octaves = octaves, BandsPerOctaves = bandsPerOctave}.Run(_numBins);
+
+            return averages;
         }
 
-        return averages;
-    }
-
-    private float CalculateAvg(float lowFreq, float hiFreq)
-    {
-        var lowBound = FreqToIndex(lowFreq);
-        var hiBound = FreqToIndex(hiFreq);
-        float avg = 0;
-        for (var i = lowBound; i <= hiBound; i++)
+        private static float CalculateAvg([ReadOnly] NativeArray<float> input, float lowFreq, float hiFreq)
         {
-            avg += _input[i];
+            var lowBound = FreqToIndex(input, lowFreq);
+            var hiBound = FreqToIndex(input, hiFreq);
+            float avg = 0;
+            for (var i = lowBound; i <= hiBound; i++)
+            {
+                avg += input[i];
+            }
+
+            avg /= (float) hiBound - (float) lowBound + 1;
+            return avg;
         }
 
-        avg /= (float) hiBound - (float) lowBound + 1;
-        return avg;
-    }
-
-    private int FreqToIndex(float freq)
-    {
-        var bandWidth = (2.0f / _input.Length) * ((float) SampleRate / 2.0f);
-
-        // Special case: freq is lower than the bandwidth of spectrum[0]
-        if (freq < bandWidth / 2) return 0;
-        // Special case: freq is within the bandwidth of spectrum[spectrum.length - 1]
-        if (freq > SampleRate / 2 - bandWidth / 2) return _input.Length - 1;
-
-        var fraction = freq / (float) SampleRate;
-        var i = (int)(_input.Length * fraction);
-        return i;
-    }
-
-    private struct AverageFftJob : IJobParallelFor
-    {
-        public void Execute(int index)
+        private static int FreqToIndex([ReadOnly] NativeArray<float> input, float freq)
         {
+            var bandWidth = (2.0f / input.Length) * ((float) SampleRate / 2.0f);
+
+            // Special case: freq is lower than the bandwidth of spectrum[0]
+            if (freq < bandWidth / 2) return 0;
+            // Special case: freq is within the bandwidth of spectrum[spectrum.length - 1]
+            if (freq > SampleRate / 2 - bandWidth / 2) return input.Length - 1;
+
+            var fraction = freq / (float) SampleRate;
+            var i = (int) (input.Length * fraction);
+            return i;
+        }
+
+        private struct AverageFftJob : IJobParallelFor
+        {
+            [ReadOnly] public NativeArray<float> Input;
+            [ReadOnly] public int Octaves, BandsPerOctaves;
+            [WriteOnly] public NativeArray<float> Averages;
+
+            public void Execute(int index)
+            {
+                for (var i = 0; i < Octaves; i++)
+                {
+                    float lowFreq;
+                    if (i == 0)
+                    {
+                        lowFreq = 0;
+                    }
+                    else
+                    {
+                        lowFreq = (SampleRate / 2) / (float) math.pow(2, Octaves - i);
+                    }
+
+                    var hiFreq = (SampleRate / 2) / (float) math.pow(2, Octaves - i - 1);
+                    var freqStep = (hiFreq - lowFreq) / BandsPerOctaves;
+                    var f = lowFreq;
+                    for (var j = 0; j < BandsPerOctaves; j++)
+                    {
+                        var offset = (int) (j + i * BandsPerOctaves);
+                        Averages[offset] = CalculateAvg(Input, f, f + freqStep);
+                        f += freqStep;
+                    }
+                }
+            }
         }
     }
 }
